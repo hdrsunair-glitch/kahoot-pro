df9a4a183a431f6b79576c99a4726f73e9b65c59
hdrsunair-glitch <hd.rs.unair@gmail.com>
2026-03-28 20:39:25 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,6 +5,10 @@ const firebaseConfig = {
   authDomain: "kahoot-clone2.firebaseapp.com",
   databaseURL: "https://kahoot-clone2-default-rtdb.firebaseio.com",
   projectId: "kahoot-clone2",
+  storageBucket: "kahoot-clone2.firebasestorage.app",
+  messagingSenderId: "667677983657",
+  appId: "1:667677983657:web:ce1f980e2deec89be77ab8",
+  measurementId: "G-9MH2PKKG9K"
 };
 
 firebase.initializeApp(firebaseConfig);
