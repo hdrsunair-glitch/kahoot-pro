4e75efb1f7cc2274870c415a03e99481dbc1bae6
hdrsunair-glitch <hd.rs.unair@gmail.com>
2026-03-29 11:08:52 +0700

    Update firebase.js to prevent multiple initializations
    Check if Firebase app is already initialized before initializing.

--- firebase.js
+++ firebase.js
@@ -5,11 +5,14 @@ const firebaseConfig = {
   authDomain: "kahoot-clone2.firebaseapp.com",
   databaseURL: "https://kahoot-clone2-default-rtdb.firebaseio.com",
   projectId: "kahoot-clone2",
-  storageBucket: "kahoot-clone2.firebasestorage.app",
+  storageBucket: "kahoot-clone2.appspot.com",
   messagingSenderId: "667677983657",
   appId: "1:667677983657:web:ce1f980e2deec89be77ab8",
   measurementId: "G-9MH2PKKG9K"
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
 const db = firebase.database();
